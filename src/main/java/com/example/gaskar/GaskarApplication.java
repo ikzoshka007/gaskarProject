a5f79e7dbf7a118879f5ac9fa1232ce8c2d0b9bc
Georgii_Bystrov <georgii_bystrov@epam.com>
2021-02-12 16:53:44 +0300

    hello

---
+++ src/main/java/com/example/gaskar/GaskarApplication.java
@@ -0,0 +1,13 @@
+package com.example.gaskar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaskarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaskarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gaskar/GaskarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gaskar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaskarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
